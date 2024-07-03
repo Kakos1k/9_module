bd0efd277397773d76a6c8dc6dbc6eac92f1d7dd
Kakos1k <168650662+Kakos1k@users.noreply.github.com>
2024-07-03 14:36:08 +0300

    init.js

--- init.js
+++ init.js
@@ -1,15 +1,3 @@
-// window.onload = function() // Функция генерации данных пользователей, путем перезагрузки страницы (отключена в проекте).
-// {
-//     const initPerson = personGenerator.getPerson();
-//     document.getElementById('surnameOutput').innerText = initPerson.surname;
-//     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-//     document.getElementById('patronymicOutput').innerText = initPerson.patronymic;
-//     document.getElementById('genderOutput').innerText = initPerson.gender;
-//     document.getElementById('birthDayOutput').innerText = initPerson.day;
-//     document.getElementById('birthMonthOutput').innerText = initPerson.month;
-//     document.getElementById('birthYearOutput').innerText = initPerson.year;
-//     document.getElementById('professionOutput').innerText = initPerson.profession;
-// };
 
 document.getElementById('generation').addEventListener('click', function() { // Метод для кнопки "Генерация"; генерирует данные пользователей
     const initPerson = personGenerator.getPerson();
